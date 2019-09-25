3d332e55035ceefab9417b237c48bcb54d6d95cd
Nathan Holland <6608414+NathanNZ@users.noreply.github.com>
2019-09-26 10:58:29 +1200

    Use a relative path when running the batch file

--- run-all.bat
+++ run-all.bat
@@ -1,5 +1,5 @@
-start dotnet run --project C:\Repos\GitHub\animal-adoption-portal\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\
-start dotnet run --project C:\Repos\GitHub\animal-adoption-portal\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\
-start dotnet run --project C:\Repos\GitHub\animal-adoption-portal\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\
-start dotnet run --project C:\Repos\GitHub\animal-adoption-portal\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\
-start dotnet run --project C:\Repos\GitHub\animal-adoption-portal\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\
+start dotnet run --project .\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\
+start dotnet run --project .\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\
+start dotnet run --project .\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\
+start dotnet run --project .\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\
+start dotnet run --project .\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\
