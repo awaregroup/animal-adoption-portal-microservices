908b6acafd7fc96f04eb2baa0dfdf583c7f034df
Nathan Holland <6608414+NathanNZ@users.noreply.github.com>
2019-12-19 16:07:01 +1300

    Support spaces in path names

--- run-all.bat
+++ run-all.bat
@@ -1,24 +1,25 @@
 dotnet dev-certs https --trust
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
 
-cd %~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\ClientApp
+cd "%~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\ClientApp"
 call npm install 
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
+cd "%~dp0"
 
-dotnet build %~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\
+dotnet build "%~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\"
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
-dotnet build %~dp0\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\
+dotnet build "%~dp0\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\"
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
-dotnet build %~dp0\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\
+dotnet build "%~dp0\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\"
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
-dotnet build %~dp0\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\
+dotnet build "%~dp0\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\"
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
-dotnet build %~dp0\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\
+dotnet build "%~dp0\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\"
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
 
-start dotnet run --project %~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\
-start dotnet run --project %~dp0\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\
-start dotnet run --project %~dp0\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\
-start dotnet run --project %~dp0\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\
-start dotnet run --project %~dp0\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\
+start dotnet run --project "%~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\"
+start dotnet run --project "%~dp0\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\"
+start dotnet run --project "%~dp0\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\"
+start dotnet run --project "%~dp0\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\"
+start dotnet run --project "%~dp0\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\"
 pause
