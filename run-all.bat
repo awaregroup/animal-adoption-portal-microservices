98e3b381dfe80abd77c2be93675b829e392194fa
Nathan Holland <6608414+NathanNZ@users.noreply.github.com>
2019-12-19 15:54:11 +1300

    Pause before exit

--- run-all.bat
+++ run-all.bat
@@ -1,23 +1,24 @@
 dotnet dev-certs https --trust
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 
 cd %~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\ClientApp
 call npm install 
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 
 dotnet build %~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 dotnet build %~dp0\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 dotnet build %~dp0\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 dotnet build %~dp0\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 dotnet build %~dp0\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
 
 start dotnet run --project %~dp0\AnimalAdoption.Web.Portal\AnimalAdoption.Web.Portal\
 start dotnet run --project %~dp0\AnimalAdoption.Service.AnimalInformation\AnimalAdoption.Service.AnimalInformation.Api\
 start dotnet run --project %~dp0\AnimalAdoption.Service.Cart\AnimalAdoption.Service.Cart.Api\
 start dotnet run --project %~dp0\AnimalAdoption.Web.Identity\AnimalAdoption.Web.Identity\
 start dotnet run --project %~dp0\AnimalAdoption.Service.Image\AnimalAdoption.Service.Image.Api\
+pause
