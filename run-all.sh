bf26b65a5b7cbf20c2a68620615d7504a2495435
Nathan Holland <6608414+NathanNZ@users.noreply.github.com>
2020-03-30 19:19:53 +1300

    .NET SDK changes cause run to be specified after project

--- run-all.sh
+++ run-all.sh
@@ -12,8 +12,8 @@ dotnet build "${SCRIPTPATH}/Cart/AnimalAdoption.Service.Cart.Api/"
 dotnet build "${SCRIPTPATH}/Identity/AnimalAdoption.Web.Identity/"
 dotnet build "${SCRIPTPATH}/Image/AnimalAdoption.Service.Image.Api/"
 
-dotnet watch run --project "${SCRIPTPATH}/Portal/AnimalAdoption.Web.Portal/" &
-dotnet watch run --project "${SCRIPTPATH}/Information/AnimalAdoption.Service.AnimalInformation.Api/" &
-dotnet watch run --project "${SCRIPTPATH}/Cart/AnimalAdoption.Service.Cart.Api/" &
-dotnet watch run --project "${SCRIPTPATH}/Identity/AnimalAdoption.Web.Identity/" &
-dotnet watch run --project "${SCRIPTPATH}/Image/AnimalAdoption.Service.Image.Api/" &
+dotnet watch --project "${SCRIPTPATH}/Portal/AnimalAdoption.Web.Portal/" run &
+dotnet watch --project "${SCRIPTPATH}/Information/AnimalAdoption.Service.AnimalInformation.Api/" run &
+dotnet watch --project "${SCRIPTPATH}/Cart/AnimalAdoption.Service.Cart.Api/" run &
+dotnet watch --project "${SCRIPTPATH}/Identity/AnimalAdoption.Web.Identity/" run &
+dotnet watch --project "${SCRIPTPATH}/Image/AnimalAdoption.Service.Image.Api/" run &
