16a2c3e729c3074ed18139e93302b9a742440e12
Nathan Holland (AWARE) <nathan.holland@awaregroup.com>
2020-05-06 20:23:02 +1200

    Move scripts around

---
+++ Scripts/run-all.cmd
@@ -0,0 +1,16 @@
+@echo off
+dotnet dev-certs https --trust
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
+
+cd "%~dp0\..\Portal\Portal.Web\ClientApp"
+call npm install 
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
+cd "%~dp0"
+
+set ASPNETCORE_ENVIRONMENT=Development
+
+start /D "%~dp0\..\Portal\Portal.Web\" dotnet watch run -c Debug
+start /D "%~dp0\..\Information\AnimalInformation.Api\" dotnet watch run -c Debug
+start /D "%~dp0\..\Cart\Cart.Api\" dotnet watch run -c Debug
+start /D "%~dp0\..\Identity\Identity.Web\" dotnet watch run -c Debug
+start /D "%~dp0\..\Image\Image.Api\" dotnet watch run -c Debug
